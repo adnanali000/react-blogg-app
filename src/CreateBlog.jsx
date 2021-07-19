537787303bab3e52a754f36ae9745d919dfd9072
adnanali000 <adnan.museel123@gmail.com>
2021-07-19 10:16:44 +0500

    create blog form

--- src/CreateBlog.jsx
+++ src/CreateBlog.jsx
@@ -2,6 +2,20 @@ const CreateBlog = () => {
     return ( 
         <div className="blogForm">
             <h2>Add New Blog</h2>
+            <form>
+                <label>Blog title: </label>
+                <input type="text" required />
+
+                <label>Blog body: </label>
+                <textarea required></textarea>
+
+                <label>Blog author: </label>
+                <select>
+                    <option value="adnan">Adnan</option>
+                    <option value="arnan">Adnan Ali</option>
+                </select>
+                <button>Add blog</button>
+            </form>
         </div>
      );
 }
