68bdee56d45bdf08c2157ab79193fcb7d57b8789
adnanali000 <adnan.museel123@gmail.com>
2021-07-19 10:35:27 +0500

    get value of form

--- src/CreateBlog.jsx
+++ src/CreateBlog.jsx
@@ -1,20 +1,37 @@
+import {useState} from 'react';
+
 const CreateBlog = () => {
+    const [title,setTitle] = useState('');
+    const [body,setBody] = useState('');
+    const [author,setAuthor] = useState('adnan');
+
     return ( 
         <div className="blogForm">
             <h2>Add New Blog</h2>
             <form>
                 <label>Blog title: </label>
-                <input type="text" required />
+                <input type="text" 
+                value = {title}
+                onChange={(e)=>setTitle(e.target.value)}  
+                required />
 
                 <label>Blog body: </label>
-                <textarea required></textarea>
+                <textarea
+                onChange={(e)=>setBody(e.target.value)}
+                required></textarea>
 
                 <label>Blog author: </label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e)=>setAuthor(e.target.value)}
+                >
                     <option value="adnan">Adnan</option>
-                    <option value="arnan">Adnan Ali</option>
+                    <option value="adnan ali">Adnan Ali</option>
                 </select>
                 <button>Add blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
             </form>
         </div>
      );
